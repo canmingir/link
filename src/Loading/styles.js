bbc107e45978ca370e3ff1811d58e0882a32720b
durulkoca <durulkoca@gmail.com>
2023-09-18 20:42:30 +0300

    Move components to new src

---
+++ src/Loading/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  stack: { width: "100%", color: "grey.500" },
+  loading: {
+    height: "7px",
+    width: "100%",
+    position: "fixed",
+    left: "0",
+    top: "66px",
+    zIndex: "1000000",
+    color: "custom.loading",
+  },
+};
+
+export default styles;
