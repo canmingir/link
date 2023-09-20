dd3844bfce65c6455833ac8f00d681bbb6bfe6c6
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-09-20 13:00:30 +0300

    Add hideTopBar prop and sideMenu icon to config.menu

--- example/config.menu.js
+++ example/config.menu.js
@@ -1,37 +1,49 @@
+import ChatIcon from "@mui/icons-material/Chat";
+import ListIcon from "@mui/icons-material/List";
+
 const menuConfig = {
   topMenu: [
-    { name: "/", url: `/`, hide: true },
+    { name: "/", url: `/`, hide: true, hideTopBar: false },
     {
-      name: "Teams",
-      url: "/teams",
+      name: "Main",
+      url: "/main",
       hide: true,
+      hideTopBar: true,
     },
     {
       name: "Chat",
       url: "/teams/chat",
       hide: true,
+      hideTopBar: false,
     },
     {
       name: "Colleagues",
       url: "/teams/colleagues",
       hide: true,
+      hideTopBar: false,
     },
     {
       name: "Colleagues",
       url: "/colleagues/:colleagueId",
       hide: true,
+      hideTopBar: false,
     },
   ],
   sideMenu: [
     {
       name: "Colleagues",
       url: "/teams/colleagues",
+      icon: ListIcon,
+      hideTopBar: false,
     },
     {
       name: "Chat",
       url: "/teams/chat",
+      icon: ChatIcon,
+      hideTopBar: true,
     },
   ],
+  topMenuColor: "custom.sidebarBG",
 };
 
 export default menuConfig;
