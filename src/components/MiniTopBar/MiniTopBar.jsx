191152337f6e660bc25d3a6889377c50f0ceb66d
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-09-20 14:12:51 +0300

    Move public media folder to example public's

--- src/components/MiniTopBar/MiniTopBar.jsx
+++ src/components/MiniTopBar/MiniTopBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import config from "../../../config";
 import styles from "./styles";
+import { useConfig } from "../../context/ConfigContext";
 
 import { AppBar, Box, Slide, Toolbar } from "@mui/material";
 
 function MiniTopBar() {
+  const globalConfig = useConfig();
   return (
     <Slide in={true} direction="right" timeout={500}>
       <AppBar position="absolute" sx={styles.appBar} variant="dense">
         <Toolbar>
-          <Box component="img" src={config.login.icon} sx={styles.logo} />
+          <Box component="img" src={globalConfig.login.icon} sx={styles.logo} />
         </Toolbar>
       </AppBar>
     </Slide>
