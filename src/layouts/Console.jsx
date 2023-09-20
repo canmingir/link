7f8775bfa39ab4ab11c0b485afa525f7dd92cd2b
durulkoca <durulkoca@gmail.com>
2023-09-20 11:46:38 +0300

    Update Console with the new TopNavBar logic

--- src/layouts/Console.jsx
+++ src/layouts/Console.jsx
@@ -3,13 +3,18 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import TopNavBar from "../components/TopNavBar";
 import config from "../../config";
+import { useConfig } from "../context/ConfigContext";
+import { useContext } from "../ContextProvider/ContextProvider";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Console() {
   const [anchorElUser, setAnchorElUser] = useState(null);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isSubmenuOpen, setSubmenuOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState();
+  const [state, dispatch] = useContext();
+  const globalConfig = useConfig();
 
   const toggleCollapse = () => {
     setIsCollapsed(!isCollapsed);
@@ -26,6 +31,23 @@ function Console() {
     setAnchorElUser(null);
   };
 
+  const handleItemSelect = (item) => {
+    dispatch({ type: "ITEM_SELECT", payload: item.id });
+  };
+
+  useEffect(() => {
+    const foundItem = globalConfig.itemsData.find(
+      (item) => item.id === state.itemId
+    );
+
+    if (foundItem) {
+      setSelectedItem(foundItem);
+    }
+    selectedItem && setSubmenuOpen(false);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <TopNavBar
@@ -34,7 +56,13 @@ function Console() {
         handleOpenUserMenu={handleOpenUserMenu}
         handleCloseUserMenu={handleCloseUserMenu}
         routes={config.topMenu}
+        itemsData={globalConfig.itemsData}
+        onItemSelect={handleItemSelect}
         sideBarToggle={toggleCollapse}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+        itemUrl="/"
+        itemName="ITEEEEEM"
       />
 
       <Box sx={{ display: "flex", flexGrow: 1 }}>
