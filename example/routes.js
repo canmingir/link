147c1a1addab9df0acd0cea2adc39302b3b90ab1
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-09-20 13:05:45 +0300

    Create main page to test new AppLayout

--- example/routes.js
+++ example/routes.js
@@ -1,5 +1,9 @@
 import Index from "./src/pages/index";
+import MainPage from "./src/pages/MainPage";
 
-const routes = [{ name: "/", url: `/`, element: Index }];
+const routes = [
+  { name: "/", url: `/`, element: Index },
+  { name: "Main", url: `/main`, element: MainPage },
+];
 
 export default routes;
--- example/src/pages/index.jsx
+++ example/src/pages/index.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Button, Container, Typography } from "@mui/material";
+
+import { Link } from "react-router-dom";
 
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <Container
+      sx={{
+        height: "100dvh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h3">Index</Typography>
+      <Button sx={{ backgroundColor: "black" }}>
+        <Link to={"/main"}>Next Page</Link>
+      </Button>
+    </Container>
+  );
 };
 
 export default Index;
