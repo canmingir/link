54381aea70e012f149f325da2528cfbaa5af7ccb
durulkoca <durulkoca@gmail.com>
2023-11-09 15:28:48 +0300

    Add more Mui components back

--- src/components/SideNavigation/SideNavigation.jsx
+++ src/components/SideNavigation/SideNavigation.jsx
@@ -1,21 +1,45 @@
-import "./style.css";
 import { Link } from "react-router-dom";
 import React from "react";
 
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
+
 export default function SideNavigation({ items = [] }) {
   return (
-    <>
-      <div className="sidebar open">
-        <ul>
-          {items.map((item) => (
-            <li key={item.name}>
-              <Link style={{ color: "white" }} to={item.url}>
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <Drawer
+      variant="permanent"
+      open={true}
+      sx={{
+        width: 250,
+        flexShrink: 0,
+        top: 64,
+        "& .MuiDrawer-paper": {
+          width: 250,
+          boxSizing: "border-box",
+          backgroundColor: "#111",
+          position: "fixed",
+          top: 64,
+          height: "100%",
+          transition: "width 0.3s",
+        },
+      }}
+    >
+      <List>
+        {items.map((item) => (
+          <ListItem
+            key={item.name}
+            component={Link}
+            to={item.url}
+            sx={{
+              color: "white",
+              "&:hover": {
+                backgroundColor: "#333",
+              },
+            }}
+          >
+            <ListItemText primary={item.name} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 }
