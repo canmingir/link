2183748ac2107eedd4f06230da7ebba9b6408f4e
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-06 22:43:26 +0300

    v0.1.9

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
+"use client";
 export { default as CompactLayout } from "./CompactLayout";
 export { default as DashboardLayout } from "./DashboardLayout";
 export { default as MainLayout } from "./MainLayout";
