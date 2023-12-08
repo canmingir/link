f11a24d6b6d1e60c4c8221c0f3ef7f4dd3a94ec8
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-08 20:33:29 +0300

    Implement SettingsProvider and Theme Provider to Platform

--- src/Platform.jsx
+++ src/Platform.jsx
@@ -1,48 +1,58 @@
+import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from "./context/ConfigContext";
 import ContextProvider from "./ContextProvider/ContextProvider";
 import GlobalSnackMessage from "./GlobalSnackMessage/GlobalSnackMessage";
 import Loading from "./Loading/Loading";
 import RouteManager from "./RouteManager/RouteManager";
+import { SettingsDrawer } from "./components/settings";
+import { SettingsProvider } from "./components/settings";
 import { SnackbarProvider } from "notistack";
+import ThemeProvider from "./theme";
 import globalConfig from "./config";
 
-import {
-  CssBaseline,
-  StyledEngineProvider,
-  ThemeProvider,
-} from "@mui/material";
 import React, { useEffect } from "react";
 import { initialState, reducer } from "./context/reducer";
 
-const Platform = ({ routes, theme, config }) => {
+const Platform = ({ routes, config }) => {
   useEffect(() => {
     globalConfig(config);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <ConfigProvider value={config}>
-            <ContextProvider reducer={reducer} state={initialState}>
-              <SnackbarProvider
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-              >
-                <Loading />
-                <GlobalSnackMessage />
-                <RouteManager routes={routes} />
-              </SnackbarProvider>
-            </ContextProvider>
-          </ConfigProvider>
-        </BrowserRouter>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <>
+      <SettingsProvider
+        defaultSettings={{
+          themeMode: "light", // 'light' | 'dark'
+          themeDirection: "ltr", //  'rtl' | 'ltr'
+          themeContrast: "default", // 'default' | 'bold'
+          themeLayout: "vertical", // 'vertical' | 'horizontal' | 'mini'
+          themeColorPresets: "default", // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
+          themeStretch: false,
+        }}
+      >
+        <ThemeProvider>
+          <BrowserRouter>
+            <ConfigProvider value={config}>
+              <ContextProvider reducer={reducer} state={initialState}>
+                <SnackbarProvider
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                >
+                  <SettingsDrawer />
+                  <Loading />
+                  <GlobalSnackMessage />
+                  <RouteManager routes={routes} />
+                </SnackbarProvider>
+              </ContextProvider>
+            </ConfigProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </SettingsProvider>
+    </>
   );
 };
 
