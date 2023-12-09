6d3bf9ed9a5c25f2cc19ddb4ac1be5ae53eb09a2
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-09 14:38:58 +0300

    Add scrollbar, settings context, and drawer option components

---
+++ src/components/settings/context/settings-provider.jsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+import { SettingsContext } from "./settings-context";
+import isEqual from "lodash/isEqual";
+import { useLocalStorage } from "../../../hooks/use-local-storage";
+
+import { useCallback, useMemo, useState } from "react";
+
+// ----------------------------------------------------------------------
+
+const STORAGE_KEY = "settings";
+
+export function SettingsProvider({ children, defaultSettings }) {
+  const { state, update, reset } = useLocalStorage(
+    STORAGE_KEY,
+    defaultSettings
+  );
+
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  // Drawer
+  const onToggleDrawer = useCallback(() => {
+    setOpenDrawer((prev) => !prev);
+  }, []);
+
+  const onCloseDrawer = useCallback(() => {
+    setOpenDrawer(false);
+  }, []);
+
+  const canReset = !isEqual(state, defaultSettings);
+
+  const memoizedValue = useMemo(
+    () => ({
+      ...state,
+      onUpdate: update,
+      // Direction
+      // Reset
+      canReset,
+      onReset: reset,
+      // Drawer
+      open: openDrawer,
+      onToggle: onToggleDrawer,
+      onClose: onCloseDrawer,
+    }),
+    [reset, update, state, canReset, openDrawer, onCloseDrawer, onToggleDrawer]
+  );
+
+  return (
+    <SettingsContext.Provider value={memoizedValue}>
+      {children}
+    </SettingsContext.Provider>
+  );
+}
+
+SettingsProvider.propTypes = {
+  children: PropTypes.node,
+  defaultSettings: PropTypes.object,
+};
