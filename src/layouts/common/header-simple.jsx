11f3035afabfb2d1b6a70613bc03cc3d2fe061ca
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-09 14:40:26 +0300

    Add common layout items

---
+++ src/layouts/common/header-simple.jsx
@@ -0,0 +1,53 @@
+import AppBar from "@mui/material/AppBar";
+import { HEADER } from "../config-layout";
+import HeaderShadow from "./header-shadow";
+import Link from "@mui/material/Link";
+import Logo from "../../components/logo";
+import SettingsButton from "./settings-button";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import { bgBlur } from "../../theme/css";
+import { useOffSetTop } from "../../hooks/use-off-set-top";
+import { useTheme } from "@mui/material/styles";
+
+// ----------------------------------------------------------------------
+
+export default function HeaderSimple() {
+  const theme = useTheme();
+
+  const offsetTop = useOffSetTop(HEADER.H_DESKTOP);
+
+  return (
+    <AppBar>
+      <Toolbar
+        sx={{
+          justifyContent: "space-between",
+          height: {
+            xs: HEADER.H_MOBILE,
+            md: HEADER.H_DESKTOP,
+          },
+          transition: theme.transitions.create(["height"], {
+            easing: theme.transitions.easing.easeInOut,
+            duration: theme.transitions.duration.shorter,
+          }),
+          ...(offsetTop && {
+            ...bgBlur({
+              color: theme.palette.background.default,
+            }),
+            height: {
+              md: HEADER.H_DESKTOP_OFFSET,
+            },
+          }),
+        }}
+      >
+        <Logo />
+
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <SettingsButton />
+        </Stack>
+      </Toolbar>
+
+      {offsetTop && <HeaderShadow />}
+    </AppBar>
+  );
+}
---
+++ src/layouts/common/settings-button.jsx
@@ -0,0 +1,61 @@
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Iconify from "../../components/iconify";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import { useSettingsContext } from "../../components/settings";
+import { varHover } from "../../components/animate";
+
+import Badge, { badgeClasses } from "@mui/material/Badge";
+
+// ----------------------------------------------------------------------
+
+export default function SettingsButton({ sx }) {
+  const settings = useSettingsContext();
+
+  return (
+    <Badge
+      color="info"
+      variant="dot"
+      invisible={!settings.canReset}
+      sx={{
+        [`& .${badgeClasses.badge}`]: {
+          top: 8,
+          right: 8,
+        },
+        ...sx,
+      }}
+    >
+      <Box
+        component={motion.div}
+        animate={{
+          rotate: [0, settings.open ? 0 : 360],
+        }}
+        transition={{
+          duration: 12,
+          ease: "linear",
+          repeat: Infinity,
+        }}
+      >
+        <IconButton
+          component={motion.button}
+          whileTap="tap"
+          whileHover="hover"
+          variants={varHover(1.05)}
+          aria-label="settings"
+          onClick={settings.onToggle}
+          sx={{
+            width: 40,
+            height: 40,
+          }}
+        >
+          <Iconify icon="solar:settings-bold-duotone" width={24} />
+        </IconButton>
+      </Box>
+    </Badge>
+  );
+}
+
+SettingsButton.propTypes = {
+  sx: PropTypes.object,
+};
