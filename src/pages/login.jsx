5cc6fb09dd3f5541c518ce476b35b57cefea6e51
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-09 14:47:03 +0300

    Edit login page for new LoginLayout

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -30,38 +30,11 @@ function LoginPage() {
 
   return (
     <Page title={`Sign in to ${config.name}`}>
-      <video
-        src={
-          "https://cdn.nucleoid.com/media/ab42486e50c5754623ace7dd2002479a.mp4"
-        }
-        autoPlay
-        loop
-        muted
-        style={{
-          transform: "scaleX(-1) scaleY(-1)",
-          height: "100%",
-          width: "100%",
-          objectFit: "cover",
-          position: "fixed",
-        }}
+      <LoginForm
+        icon={config.login.icon}
+        name={config.login.name}
+        formColor={formColor}
       />
-      <Box
-        sx={{
-          position: "absolute",
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <LoginForm
-          icon={config.login.icon}
-          name={config.login.name}
-          formColor={formColor}
-        />
-      </Box>
     </Page>
   );
 }
