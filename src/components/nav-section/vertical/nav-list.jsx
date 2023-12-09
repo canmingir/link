4e1fe6c1f95b3dd12fe94312b687785477fddb64
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-09 14:39:49 +0300

    Add nav section component

---
+++ src/components/nav-section/vertical/nav-list.jsx
@@ -0,0 +1,82 @@
+import Collapse from "@mui/material/Collapse";
+import NavItem from "./nav-item";
+import PropTypes from "prop-types";
+import { useActiveLink } from "../../../routes/hooks";
+import { usePathname } from "../../../routes/hooks";
+
+import { useCallback, useEffect, useState } from "react";
+
+// ----------------------------------------------------------------------
+
+export default function NavList({ data, depth }) {
+  const pathname = usePathname();
+  const active = useActiveLink(data.path, !!data.children);
+  const [openMenu, setOpenMenu] = useState(active);
+  useEffect(() => {
+    if (!active) {
+      handleCloseMenu();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
+
+  const handleToggleMenu = useCallback(() => {
+    if (data?.children) {
+      setOpenMenu((prev) => !prev);
+    }
+  }, [data?.children]);
+
+  const handleCloseMenu = useCallback(() => {
+    setOpenMenu(false);
+  }, []);
+  return (
+    <>
+      <NavItem
+        open={openMenu}
+        onClick={handleToggleMenu}
+        //
+        title={data?.title}
+        path={data?.path}
+        icon={data?.icon}
+        info={data?.info}
+        roles={data?.roles}
+        caption={data?.caption}
+        disabled={data?.disabled}
+        //
+        depth={depth}
+        hasChild={!!data?.children}
+        externalLink={data?.path?.includes("http")}
+        //
+        active={active}
+        className={active ? "active" : ""}
+      />
+
+      {!!data.children && (
+        <Collapse in={openMenu} unmountOnExit>
+          <NavSubList data={data.children} depth={depth} />
+        </Collapse>
+      )}
+    </>
+  );
+}
+
+NavList.propTypes = {
+  data: PropTypes.object,
+  depth: PropTypes.number,
+};
+
+// ----------------------------------------------------------------------
+
+function NavSubList({ data, depth }) {
+  return (
+    <>
+      {data?.map((list) => (
+        <NavList key={list?.title} data={list} depth={depth + 1} />
+      ))}
+    </>
+  );
+}
+
+NavSubList.propTypes = {
+  data: PropTypes.array,
+  depth: PropTypes.number,
+};
