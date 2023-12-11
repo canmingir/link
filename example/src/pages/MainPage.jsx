663e39c5789b545da922b3e8ece1031fe2c1a290
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-11 20:13:32 +0300

    Update example project for test new components

--- example/src/pages/MainPage.jsx
+++ example/src/pages/MainPage.jsx
@@ -1,7 +1,41 @@
 import { Box, Container, Typography } from "@mui/material";
 import { alpha } from "@mui/material/styles";
-
+import Chart, { useChart } from "../../../components";
 function MainPage() {
+  const series = [44, 55, 13, 10];
+  const chartOptions = useChart({
+    labels: ["Team A", "Team B", "Team C", "Team D"],
+    legend: {
+      position: "right",
+      offsetX: -20,
+      offsetY: 64,
+      itemMargin: {
+        vertical: 8,
+      },
+    },
+    stroke: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: true,
+      dropShadow: {
+        enabled: false,
+      },
+    },
+    tooltip: {
+      fillSeriesColor: false,
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: false,
+          },
+        },
+      },
+    },
+  });
+
   return (
     <Container>
       <Typography variant="h3">Main</Typography>{" "}
@@ -14,6 +48,14 @@ function MainPage() {
           bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
           border: (theme) => `dashed 1px ${theme.palette.divider}`,
         }}
+      />
+      <Chart
+        dir="ltr"
+        type="pie"
+        series={series}
+        options={chartOptions}
+        width={400}
+        height="auto"
       />
     </Container>
   );
--- example/src/pages/SecondPage.jsx
+++ example/src/pages/SecondPage.jsx
@@ -1,31 +1,60 @@
-import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-
+import ItemCard from "../../../components/LargeItemCard";
+import config from "../../config";
 export default function SecondPage() {
+  const items = [
+    {
+      id: "1",
+      name: "Software Team",
+      createdAt: new Date(),
+      icon: ":box:",
+    },
+  ];
+
+  const subItem = [
+    {
+      id: "1",
+      name: "Software Team",
+      createdAt: new Date(),
+    },
+    {
+      id: "2",
+      name: "Design Team",
+      createdAt: new Date(),
+    },
+    {
+      id: "3",
+      name: "Marketing Team",
+      createdAt: new Date(),
+    },
+    {
+      id: "4",
+      name: "HR Team",
+      createdAt: new Date(),
+    },
+  ];
   return (
     <Stack spacing={1} alignItems="center">
-      <Button variant="contained">Button</Button>
-      <Button variant="contained" color="primary">
-        Button
-      </Button>
-      <Button variant="contained" color="secondary">
-        Button
-      </Button>
-      <Button variant="contained" color="info">
-        Button
-      </Button>
-      <Button variant="contained" color="success">
-        Button
-      </Button>
-      <Button variant="contained" color="warning">
-        Button
-      </Button>
-      <Button variant="contained" color="error">
-        Button
-      </Button>
-      <Button disabled variant="contained">
-        Button
-      </Button>
+      <Box
+        gap={3}
+        display="grid"
+        gridTemplateColumns={{
+          xs: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(3, 1fr)",
+        }}
+      >
+        {items.map((item) => (
+          <ItemCard
+            key={item.id}
+            item={item}
+            subItem={subItem}
+            subItemName="Services"
+            base={config.base}
+          />
+        ))}
+      </Box>
     </Stack>
   );
 }
