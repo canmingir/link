73b4998ba5587e026ba0833f18aaf514f2017b7c
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-11 20:05:43 +0300

    Add TwoSideLayout to Layouts

---
+++ src/layouts/TwoSideLayout/TwoSideLayout.jsx
@@ -0,0 +1,31 @@
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2";
+import React from "react";
+import Typography from "@mui/material/Typography";
+// ----------------------------------------------------------------------
+
+export default function TwoSideLayout({ rows, title }) {
+  const rowData = rows;
+
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h4" sx={{ mb: 5 }}>
+        {title}
+      </Typography>
+
+      {Object.values(rowData).map((array, index) => {
+        let eleman_sayisi = array.length;
+        let grid_sayisi = 12 / eleman_sayisi;
+        return (
+          <Grid container spacing={3} key={index}>
+            {array.map((component, i) => (
+              <Grid item xs={12} sm={6} md={grid_sayisi} key={i}>
+                {component}
+              </Grid>
+            ))}
+          </Grid>
+        );
+      })}
+    </Container>
+  );
+}
