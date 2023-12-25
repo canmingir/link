11b73451584b50048dacd70b66d52399ad216692
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-25 17:02:00 +0300

    Implement SelectBar to SimpleLayout

--- src/layouts/SimpleLayout/SimpleLayout.jsx
+++ src/layouts/SimpleLayout/SimpleLayout.jsx
@@ -2,13 +2,36 @@ import Header from "../common/header-simple";
 import { Outlet } from "react-router";
 import PropTypes from "prop-types";
 import React from "react";
-
+import { useConfig } from "../../context/ConfigContext";
+import { useContext } from "../../ContextProvider/ContextProvider";
 // ----------------------------------------------------------------------
 
 export default function SimpleLayout() {
+  const [state, dispatch] = useContext();
+  const [selectedItem, setSelectedItem] = React.useState();
+  const globalConfig = useConfig();
+
+  React.useEffect(() => {
+    const foundItem = globalConfig.itemsData.find(
+      (item) => item.id === state.itemId
+    );
+
+    if (foundItem) {
+      setSelectedItem(foundItem);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.itemId]);
+  const handleItemSelect = (item) => {
+    dispatch({ type: "ITEM_SELECT", payload: item.id });
+  };
   return (
     <>
-      <Header />
+      <Header
+        handleItemSelect={handleItemSelect}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
 
       <Outlet />
     </>
