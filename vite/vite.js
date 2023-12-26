bd0ad431f2e34e93fa8b641c77044284080f052f
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2023-12-26 21:03:04 +0300

    Add esbuildOptions and resolve aliases in vite.js

--- vite/vite.js
+++ vite/vite.js
@@ -1,15 +1,35 @@
 import globalConfig from "../src/config.js";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-async function vite() {
+function vite() {
   const config = globalConfig();
 
   return {
     base: config.base,
     plugins: [react(), svgr()],
     optimizeDeps: {
+      esbuildOptions: {
+        jsx: "automatic",
+      },
       include: ["@mui/material"],
+    },
+
+    resolve: {
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), "node_modules/$1"),
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(
+            process.cwd(),
+            "/node_modules/platform-npm/template/src/$1"
+          ),
+        },
+      ],
     },
   };
 }
