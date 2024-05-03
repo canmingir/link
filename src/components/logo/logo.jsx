b41f331792b8b2d8330b3eda495832b5d156352e
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-03 21:00:01 +0300

    Add default logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -3,14 +3,14 @@ import Link from "@mui/material/Link";
 import PropTypes from "prop-types";
 import React from "react";
 import { RouterLink } from "../../routes/components";
-
+import config from "../../../../../config";
 // ----------------------------------------------------------------------
 
 const Logo = ({ disabledLink = false, sx }) => {
   const logo = (
     <Box
       component="img"
-      src="./media/logo.png"
+      src={config.logo || "https://cdn.nucleoid.com/media/icon.png"}
       sx={{ width: 40, height: 40, cursor: "pointer", ...sx }}
     />
   );
