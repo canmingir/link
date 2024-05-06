4bc3915dc54ed92c23133000ce920f046d6c3431
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-06 23:52:40 +0300

    Create getUserDetails instance

--- src/http/user.js
+++ src/http/user.js
@@ -27,4 +27,36 @@ instance.interceptors.request.use(async (request) => {
   return request;
 });
 
+instance.getUserDetails = async () => {
+  const refreshToken = await storage.get(getProjectName(), "refreshToken");
+
+  if (refreshToken) {
+    try {
+      const response = await axios.get(
+        config.login?.github.userUrl || config.login?.google.userUrl,
+        {
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        }
+      );
+      if (config.login.github.userUrl) {
+        return {
+          name: response.data.login,
+          avatarUrl: response.data.avatar_url,
+        };
+      } else if (config.login.google.userUrl) {
+        return {
+          name: response.data.name,
+          avatarUrl: response.data.picture,
+        };
+      }
+    } catch (error) {
+      console.error("Failed to fetch user details:", error);
+      throw error;
+    }
+  }
+  return null;
+};
+
 export default instance;
