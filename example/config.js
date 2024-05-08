2e035d7d899c8e913e28c82df1e594673926aed1
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-08 21:53:26 +0300

    Refactor example project's config-menu and config

--- example/config.js
+++ example/config.js
@@ -1,50 +1,27 @@
-import { Button } from "@mui/material";
-import ConsoleButton from "./src/widgets/ConsoleButton";
-import menuConfig from "./config.menu";
-
 const config = {
-  base: "/AAA", //Optional
-  api: "http://localhost:3001", //Optional but login doesn't work without it
+  name: "Nucleoid.com",
+  base: "/",
+  env: "dev",
+  api: "http://localhost:3000",
   login: {
-    //Mandatory
     icon: "/media/logo.png",
     largeIcon: "/media/largeLogo.png",
-    name: "Gray Collar Dashboard",
+    name: "Nucleoid.com",
     nucleoid: {},
     github: {
       authUrl: "https://github.com/login/oauth/authorize",
       clientId: "0c2844d3d19dc9293fc5",
       redirectUri: "http://localhost:5173/callback",
+      userUrl: "https://api.github.com/user",
       scope: "user",
       response_type: "code",
     },
   },
   settings: {
     mode: "dark", // 'light' | 'dark'
+    colorPresets: "cyan", // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
   },
-  ...menuConfig,
-  itemsData: [
-    {
-      id: 1,
-      title: "Project X",
-      icon: "tdesign/anchor",
-    },
-    {
-      id: 2,
-      title: "Project Y",
-      icon: "material-symbols/wine-bar",
-    },
-    {
-      id: 3,
-      title: "Deneme",
-      icon: "material-symbols/wind-power-sharp",
-    },
-    {
-      id: 4,
-      title: "Fasign",
-      icon: "material-symbols/whatshot",
-    },
-  ],
+  itemsPath: "/projects",
 };
 
 export default config;
--- example/config.menu.js
+++ example/config.menu.js
@@ -1,78 +1,32 @@
-import ActionButton from "./src/components/ActionButton";
 const menuConfig = {
+  topMenu: [],
   sideMenu: [
     {
-      subheader: "Pages",
+      subheader: "Service",
       items: [
         {
-          title: "First Page",
-          icon: "solar:fire-minimalistic-bold-duotone",
-          path: "/",
+          title: "Service",
+          icon: "solar:code-2-bold-duotone",
+          path: "/service",
         },
         {
-          title: "Main",
-          icon: "solar:home-2-bold-duotone",
-          path: "/main",
+          title: "Reports",
+          icon: "solar:chart-line-duotone",
+          path: "/service/reports",
         },
         {
-          title: "Second Page",
-          icon: "solar:document-bold-duotone",
-          path: "/secondPage",
-        },
-      ],
-    },
-    {
-      subheader: "SUBLISTS",
-      items: [
-        {
-          title: "user",
-          path: "/user",
-          icon: "solar:users-group-rounded-bold-duotone",
-          children: [
-            {
-              title: "profile",
-              path: "/user/profile",
-              icon: "solar:shield-user-bold-duotone",
-            },
-            {
-              title: "list",
-              path: "/user/list",
-              icon: "solar:list-bold-duotone",
-            },
-            {
-              title: "create",
-              path: "/user/create",
-              icon: "solar:add-circle-bold-duotone",
-            },
-            {
-              title: "delete",
-              path: "/user/delete",
-              icon: "solar:trash-bin-2-bold-duotone",
-            },
-          ],
+          title: "Logs",
+          icon: "solar:course-up-bold-duotone",
+          path: "/service/logs",
         },
       ],
     },
   ],
-
-  topMenu: [
-    {
-      title: "Index",
-      icon: "solar:home-2-bold-duotone",
-      path: "/",
-    },
-    {
-      title: "Main",
-      icon: "solar:home-2-bold-duotone",
-      path: "/main",
-    },
-    {
-      title: "SecondPage",
-      icon: "solar:atom-bold-duotone",
-      path: "/secondPage",
-    },
-  ],
-
+  endItem: {
+    title: "Project Settings",
+    icon: "ic:baseline-settings",
+    path: "/settings",
+  },
   options: [
     {
       label: "Home",
@@ -87,17 +41,7 @@ const menuConfig = {
       linkTo: "/",
     },
   ],
-
-  itemBar: [
-    {
-      itemName: "item",
-      addNewItem: function addNew() {},
-      itemsData: [{ id: 1, title: "item1", icon: ":box:" }],
-    },
-  ],
-
-  actionButtons: [ActionButton],
-  fullScreenLayout: "left",
+  actionButtons: [],
 };
 
 export default menuConfig;
