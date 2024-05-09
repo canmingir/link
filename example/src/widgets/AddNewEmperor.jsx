e35fb1c55334c6c9775ea7f4f6fd4786b3c07f85
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-09 21:21:13 +0300

    Create AddNewEmperor and use platform events

---
+++ example/src/widgets/AddNewEmperor.jsx
@@ -0,0 +1,117 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import { publish, useEvent } from "@nucleoidjs/react-event";
+
+function AddNewEmperor() {
+  // TODO refactor this event (ADD_NEW_DIALOG_OPENED and ADD_NEW_DIALOG_CLOSED)
+  const [addNewDialog] = useEvent("ADD_NEW_DIALOG_OPENED", { open: false });
+  const [emperor, setEmperor] = useState({
+    id: "",
+    name: "",
+    born: "",
+    icon: "",
+    reign: "",
+    portrait: "",
+    description: "",
+  });
+
+  const handleClose = () => {
+    publish("ADD_NEW_DIALOG_OPENED", { open: false });
+  };
+
+  const handleChange = (e) => {
+    setEmperor({ ...emperor, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(emperor);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={addNewDialog.open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Add New Emperor</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please fill out the details of the new emperor.
+          </DialogContentText>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="name"
+              label="Name"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="born"
+              label="Born"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="icon"
+              label="Icon"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="reign"
+              label="Reign"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="portrait"
+              label="Portrait URL"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="description"
+              label="Description"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+            />
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button type="submit" color="primary">
+                Add
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddNewEmperor;
