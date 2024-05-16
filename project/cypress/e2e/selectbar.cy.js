0c313d7a87fd240c56e711af04914477a3f39908
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-16 20:09:12 +0300

    Add intercept and fixtures to selectbar spec

--- project/cypress/e2e/selectbar.cy.js
+++ project/cypress/e2e/selectbar.cy.js
@@ -1,6 +1,27 @@
 describe("SelectBar", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
+
+    cy.fixture("EMPEROR/EMPERORS_GET.json").then((emperors) => {
+      cy.intercept("GET", "http://localhost:3000/emperors", {
+        statusCode: 200,
+        body: emperors.twoEmperors,
+      });
+    });
+
+    cy.fixture("EMPEROR/EMPERORS_POST.json").then((emperor) => {
+      cy.intercept("POST", "http://localhost:3000/emperors", {
+        statusCode: 200,
+        body: emperor,
+      });
+    });
+
+    cy.fixture("EMPEROR/EMPERORS_GET.json").then((emperors) => {
+      cy.intercept("GET", "http://localhost:3000/emperors", {
+        statusCode: 200,
+        body: emperors.threeEmperors,
+      });
+    });
   });
 
   it("should list emperors", () => {
@@ -25,12 +46,14 @@ describe("SelectBar", () => {
     cy.getBySel("open-select-bar-button").click();
     cy.getBySel("add-new-item-button").click();
 
-    cy.get('input[name="name"]').type("Emperor Name");
-    cy.get('input[name="born"]').type("Born Date");
-    cy.get('input[name="icon"]').type("Icon URL");
-    cy.get('input[name="reign"]').type("Reign Period");
-    cy.get('input[name="portrait"]').type("Portrait URL");
-    cy.get('input[name="description"]').type("Description");
+    cy.fixture("EMPEROR/EMPERORS_POST.json").then((emperor) => {
+      cy.get('input[name="name"]').type(emperor.name);
+      cy.get('input[name="born"]').type(emperor.born);
+      cy.get('input[name="icon"]').type(emperor.icon);
+      cy.get('input[name="reign"]').type(emperor.reign);
+      cy.get('input[name="portrait"]').type(emperor.portrait);
+      cy.get('input[name="description"]').type(emperor.description);
+    });
 
     cy.get('button[type="submit"]').click();
   });
