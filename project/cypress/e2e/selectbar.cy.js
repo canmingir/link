4f8593971f07b538783c6b43722f1a495f169e56
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-16 20:24:17 +0300

    Do controls dynamic by connecting them to fixtures

--- project/cypress/e2e/selectbar.cy.js
+++ project/cypress/e2e/selectbar.cy.js
@@ -1,52 +1,67 @@
 describe("SelectBar", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:5173");
+    cy.fixture("EMPEROR/EMPERORS_POST.json").as("newEmperor");
+    cy.fixture("EMPEROR/EMPERORS_GET.json").as("emperors");
 
-    cy.fixture("EMPEROR/EMPERORS_GET.json").then((emperors) => {
+    cy.get("@emperors").then((emperors) => {
       cy.intercept("GET", "http://localhost:3000/emperors", {
         statusCode: 200,
         body: emperors.twoEmperors,
       });
     });
 
-    cy.fixture("EMPEROR/EMPERORS_POST.json").then((emperor) => {
+    cy.get("@newEmperor").then((emperor) => {
       cy.intercept("POST", "http://localhost:3000/emperors", {
         statusCode: 200,
         body: emperor,
       });
     });
 
-    cy.fixture("EMPEROR/EMPERORS_GET.json").then((emperors) => {
-      cy.intercept("GET", "http://localhost:3000/emperors", {
-        statusCode: 200,
-        body: emperors.threeEmperors,
-      });
-    });
+    cy.visit("http://localhost:5173");
   });
 
   it("should list emperors", () => {
     cy.getBySel("open-select-bar-button").click();
     cy.getBySel("select-bar-dialog").should("be.visible");
+
     cy.getBySel("item-list");
-    cy.getBySel("item-button").should("have.length", 3);
+
+    cy.get("@emperors").then((emperors) => {
+      cy.getBySel("item-button").should(
+        "have.length",
+        emperors.twoEmperors.length
+      );
+      cy.getBySel("item-button").contains(emperors.twoEmperors[0].name);
+      cy.getBySel("item-button").contains(emperors.twoEmperors[1].name);
+    });
   });
+
   it("should select an emperor", () => {
     cy.getBySel("open-select-bar-button").click();
     cy.getBySel("item-button").first().click();
     cy.checkStorage("itemId", "1");
     cy.contains("Marcus Aurelius");
   });
+
   it("should search for an emperor", () => {
     cy.getBySel("open-select-bar-button").click();
     cy.getBySel("item-input").type("marcus");
     cy.getBySel("item-button").should("have.length", 1);
     cy.getBySel("item-button").contains("Marcus Aurelius");
   });
-  it.only("should add a new emperor", () => {
+
+  it("should add a new emperor", () => {
+    cy.get("@emperors").then((emperors) => {
+      cy.intercept("GET", "http://localhost:3000/emperors", {
+        statusCode: 200,
+        body: emperors.threeEmperors,
+      });
+    });
+
     cy.getBySel("open-select-bar-button").click();
     cy.getBySel("add-new-item-button").click();
 
-    cy.fixture("EMPEROR/EMPERORS_POST.json").then((emperor) => {
+    cy.get("@newEmperor").then((emperor) => {
       cy.get('input[name="name"]').type(emperor.name);
       cy.get('input[name="born"]').type(emperor.born);
       cy.get('input[name="icon"]').type(emperor.icon);
@@ -56,5 +71,10 @@ describe("SelectBar", () => {
     });
 
     cy.get('button[type="submit"]').click();
+    cy.getBySel("item-button").should("have.length", 3);
+
+    cy.get("@newEmperor").then((emperor) => {
+      cy.getBySel("item-button").contains(emperor.name);
+    });
   });
 });
