a02ea587f00b155f857bdf4fdb83b7f738e9da8a
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-17 17:05:17 +0300

    Create FullScreenLayout test

--- project/cypress/e2e/layouts.cy.js
+++ project/cypress/e2e/layouts.cy.js
@@ -10,3 +10,18 @@ describe("CompactLayout", () => {
   });
 });
 
+describe("FullScreenLayout", () => {
+  beforeEach(() => {
+    cy.fixture("CONFIG/MENU_CONFIG.js").as("config");
+  });
+
+  it("should contain only mini nav ", () => {
+    cy.visit("http://localhost:5173/emperor/battle");
+    cy.getBySel("header").should("not.exist");
+    cy.getBySel("dashboard-layout-header").should("not.exist");
+    cy.getBySel("dashboard-layout-nav-horizontal").should("not.exist");
+    cy.getBySel("nav-mini").should("be.exist");
+    cy.getBySel("nav-horizontal").should("not.exist");
+    cy.getBySel("nav-vertical").should("not.exist");
+  });
+});
