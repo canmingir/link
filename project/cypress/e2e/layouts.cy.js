767a15ca5dfde3fc31f6bb93848bde6b04eb43cc
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-17 17:15:08 +0300

    Create mobile test for CompactLayout

--- project/cypress/e2e/layouts.cy.js
+++ project/cypress/e2e/layouts.cy.js
@@ -8,9 +8,19 @@ describe("CompactLayout", () => {
     cy.getBySel("nav-horizontal").should("not.exist");
     cy.getBySel("nav-vertical").should("not.exist");
   });
+  it("should contain only header (mobile)", () => {
+    cy.viewport(375, 667);
+    cy.visit("http://localhost:5173");
+    cy.getBySel("header").should("be.exist");
+    cy.getBySel("dashboard-layout-header").should("not.exist");
+    cy.getBySel("dashboard-layout-nav-horizontal").should("not.exist");
+    cy.getBySel("nav-mini").should("not.exist");
+    cy.getBySel("nav-horizontal").should("not.exist");
+    cy.getBySel("nav-vertical").should("not.exist");
+  });
 });
 
-describe("FullScreenLayout", () => {
+describe.only("FullScreenLayout", () => {
   beforeEach(() => {
     cy.fixture("CONFIG/MENU_CONFIG.js").as("config");
   });
