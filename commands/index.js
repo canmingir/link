926715fb6272e1da986c3c52061a2bb56b4ec743
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-05-20 21:35:38 +0300

    Create commands file

---
+++ commands/index.js
@@ -0,0 +1,56 @@
+Cypress.Commands.add("checkStorage", (key, expectedValue) => {
+  cy.window().should((win) => {
+    const actualValue = win.localStorage.getItem(key);
+    expect(actualValue).to.eq(expectedValue);
+  });
+});
+
+Cypress.Commands.add("getBySel", (selector, ...args) => {
+  return cy.get(`[data-cy=${selector}]`, ...args);
+});
+
+Cypress.Commands.add("checkLayout", (layout) => {
+  const layoutMapping = {
+    CompactLayout: {
+      shouldExist: ["header"],
+      shouldNotExist: [
+        "dashboard-layout-header",
+        "dashboard-layout-nav-horizontal",
+        "nav-mini",
+        "nav-horizontal",
+        "nav-vertical",
+      ],
+    },
+    FullScreenLayout: {
+      shouldExist: ["nav-mini"],
+      shouldNotExist: [
+        "header",
+        "dashboard-layout-header",
+        "dashboard-layout-nav-horizontal",
+        "nav-horizontal",
+        "nav-vertical",
+      ],
+    },
+    DashboardLayout: {
+      shouldExist: ["dashboard-layout-header", "nav-vertical"],
+      shouldNotExist: [
+        "header",
+        "dashboard-layout-nav-horizontal",
+        "nav-mini",
+        "nav-horizontal",
+      ],
+    },
+  };
+
+  const layoutChecks = layoutMapping[layout];
+
+  if (layoutChecks) {
+    layoutChecks.shouldExist.forEach((element) => {
+      cy.getBySel(element).should("be.exist");
+    });
+
+    layoutChecks.shouldNotExist.forEach((element) => {
+      cy.getBySel(element).should("not.exist");
+    });
+  }
+});
