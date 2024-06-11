cd8233c0c6ec836339f1805aabf5aecbf83c3fdf
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-11 22:00:31 +0300

    Add dynamic import for configs and validate them

--- src/config/config.js
+++ src/config/config.js
@@ -1,21 +1,46 @@
 import { ConfigSchema } from "./ConfigSchema.js";
-import mainConfig from "../../../../config.js";
-let _config = {};
+import { MenuConfigSchema } from "./MenuConfigSchema.js";
+import { TemplateConfigSchema } from "./TemplateConfigSchema.js";
+
+let _mainConfig = {};
+let _menuConfig = {};
+let _templateConfig = {};
 
 const config = {
-  init: function () {
-    const { value, error } = ConfigSchema.validate(mainConfig);
-    if (error) {
-      throw error;
+  init: async function () {
+    const configMenu = await import("../../../../config.menu.js");
+    const configTemplate = await import("../../../../config.template.js");
+    const configMain = await import("../../../../config.js");
+
+    const { value: mainConfig, error: errorConfig } =
+      await ConfigSchema.validate(configMain.default);
+    const { value: menuConfig, error: errorMenu } =
+      await MenuConfigSchema.validate(configMenu.default);
+    const { value: templateConfig, error: errorTemplate } =
+      await TemplateConfigSchema.validate(configTemplate.default);
+
+    if (errorConfig || errorMenu || errorTemplate) {
+      throw errorConfig || errorMenu || errorTemplate;
     }
-    _config = value;
+
+    _mainConfig = mainConfig;
+    _menuConfig = menuConfig;
+    _templateConfig = templateConfig;
+
+    const config = {
+      ..._mainConfig,
+      menu: _menuConfig,
+      template: _templateConfig,
+    };
+
+    return config;
   },
-  vite: function () {
-    const { value, error } = ConfigSchema.validate(mainConfig);
+  vite: async function () {
+    const mainConfig = await import("../../../../config.js");
+    const { value, error } = ConfigSchema.validate(mainConfig.default);
     return { error, value };
   },
   get: function () {
-    return _config;
   },
 };
 
