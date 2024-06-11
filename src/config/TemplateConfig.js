5c3cd869a7f4f7d9568e2199ea410edbc57b5f0b
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-11 20:33:00 +0300

    Add TemplateSchema

---
+++ src/config/TemplateConfig.js
@@ -0,0 +1,29 @@
+import Joi from "joi";
+
+export const TemplateConfig = Joi.object({
+  login: Joi.object({
+    variant: Joi.string()
+      .valid("classic", "modern")
+      .optional()
+      .default("classic"),
+    image: Joi.string()
+      .optional()
+      .default("https://minimals.cc/assets/background/overlay_3.jpg"),
+    icon: Joi.string()
+      .optional()
+      .default("https://cdn.nucleoid.com/media/icon.png"),
+    largeIcon: Joi.string()
+      .optional()
+      .default("https://cdn.nucleoid.com/media/icon.png"),
+  }).required(),
+  settings: Joi.object({
+    mode: Joi.string().valid("light", "dark").optional().default("dark"),
+    colorPresets: Joi.string()
+      .valid("default", "cyan", "purple", "blue", "orange", "red")
+      .optional()
+      .default("default"),
+  })
+    .optional()
+    .default({ mode: "dark", colorPresets: "cyan" }),
+  itemsPath: Joi.string().required(),
+});
