9bea9df37e13136515965f668bc8492c4fdfd453
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-13 18:27:35 +0300

    Add ReactDOM.create and config.init to init function

--- src/Platform.jsx
+++ src/Platform.jsx
@@ -1,70 +1,21 @@
-import "./global.css";
-
-import ContextProvider from "./ContextProvider/ContextProvider";
-import GlobalSnackMessage from "./GlobalSnackMessage/GlobalSnackMessage";
-import Loading from "./Loading/Loading";
-import React from "react";
-import RouteManager from "./RouteManager/RouteManager";
-
-import { SettingsDrawer } from "./components/settings";
-import { SettingsProvider } from "./components/settings";
-import { SnackbarProvider } from "notistack";
-import ThemeProvider from "./theme";
 import config from "./config/config";
+import oauth from "./http/oauth";
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import routes from "../../../routes";
 
-import { BrowserRouter, Navigate } from "react-router-dom";
-import { initialState, reducer } from "./context/reducer";
-import { publish, subscribe, useEvent } from "@nucleoidai/react-event";
+const Platform = {
+  init: function () {
+    config.init();
 
-window["@nucleoidai"] = {
-  Event: { publish, subscribe, useEvent },
-};
-
-config.init();
-
-const Platform = ({ routes, dialogs }) => {
-  const [configInitError] = useEvent("CONFIG_INITIALIZE_FAILED", {
-    error: "",
-    file: "",
-  });
-
-  const { base, template } = config.get();
-  return (
-    <>
-      <SettingsProvider
-        defaultSettings={{
-          themeMode: template.theme.mode || "dark",
-          themeDirection: "ltr",
-          themeContrast: "default",
-          themeLayout: "vertical",
-          themeColorPresets: template.theme.colorPresets || "default",
-          themeStretch: false,
-        }}
-      >
-        <ThemeProvider>
-          <BrowserRouter basename={base}>
-            {configInitError && (
-              <Navigate to="/config-error" state={configInitError} />
-            )}
-            <ContextProvider reducer={reducer} state={initialState}>
-              <SnackbarProvider
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-              >
-                {dialogs && dialogs()}
-                <SettingsDrawer />
-                <Loading />
-                <GlobalSnackMessage />
-                <RouteManager routes={routes} />
-              </SnackbarProvider>
-            </ContextProvider>
-          </BrowserRouter>
-        </ThemeProvider>
-      </SettingsProvider>
-    </>
-  );
+    oauth.defaults.baseURL = config.get().api;
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <App routes={routes} />
+      </React.StrictMode>
+    );
+  },
 };
 
 export default Platform;
