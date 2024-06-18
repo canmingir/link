008cdd992543d10c2184a57a074c81bf762c90e7
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-18 17:09:26 +0300

    Remove Platform.jsx and rename App to Platform

--- src/Platform.jsx
+++ src/Platform.jsx
@@ -1,21 +1,76 @@
-import config from "./config/config";
-import oauth from "./http/oauth";
-import App from "./App";
+import { init } from "./config/config";
+init();
+
+import "./global.css";
+
+import { BrowserRouter, Navigate } from "react-router-dom";
+import { initialState, reducer } from "./context/reducer";
+import { publish, subscribe, useEvent } from "@nucleoidai/react-event";
+
+import ContextProvider from "./ContextProvider/ContextProvider";
+import GlobalSnackMessage from "./GlobalSnackMessage/GlobalSnackMessage";
+import Loading from "./Loading/Loading";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import routes from "../../../routes";
+import RouteManager from "./RouteManager/RouteManager";
+import { SettingsDrawer } from "./components/settings";
+import { SettingsProvider } from "./components/settings";
+import { SnackbarProvider } from "notistack";
+import ThemeProvider from "./theme";
+import oauth from "./http/oauth";
+import config from "./config/config";
 
-const Platform = {
-  init: function () {
-    config.init();
+window["@nucleoidai"] = {
+  Event: { publish, subscribe, useEvent },
+};
 
-    oauth.defaults.baseURL = config.get().api;
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <App routes={routes} />
-      </React.StrictMode>
-    );
-  },
+setTimeout(() => {
+  const { api } = config();
+
+  oauth.defaults.baseURL = api;
+}, 0);
+
+const Platform = ({ routes }) => {
+  const [configInitError] = useEvent("CONFIG_INITIALIZE_FAILED", {
+    error: "",
+    file: "",
+  });
+
+  const { base, template } = config();
+  return (
+    <>
+      <SettingsProvider
+        defaultSettings={{
+          themeMode: template.theme.mode || "dark",
+          themeDirection: "ltr",
+          themeContrast: "default",
+          themeLayout: "vertical",
+          themeColorPresets: template.theme.colorPresets || "default",
+          themeStretch: false,
+        }}
+      >
+        <ThemeProvider>
+          <BrowserRouter basename={base}>
+            {configInitError.error !== "" && (
+              <Navigate to="/config-error" state={configInitError} />
+            )}
+            <ContextProvider reducer={reducer} state={initialState}>
+              <SnackbarProvider
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+              >
+                <SettingsDrawer />
+                <Loading />
+                <GlobalSnackMessage />
+                <RouteManager routes={routes} />
+              </SnackbarProvider>
+            </ContextProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </SettingsProvider>
+    </>
+  );
 };
 
 export default Platform;
