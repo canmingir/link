6d2b7a8e57c8231d690cc3cfbca2b4e3a4cabf0c
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-06-18 13:55:50 +0300

    Use new config hook in useItemState

--- src/hooks/useItemsState.js
+++ src/hooks/useItemsState.js
@@ -1,16 +1,18 @@
 import axios from "axios";
-import config from "../../../../config.js";
+import config from "../config/config";
 import useSWR from "swr";
 
+const { api, itemsPath } = config.get();
+
 const instance = axios.create({
-  baseURL: config.api,
+  baseURL: api,
 });
 
 const fetcher = (url) => instance.get(url).then((res) => res.data);
 
 export const useProject = () => {
   const GetItems = () => {
-    const path = config.itemsPath;
+    const path = itemsPath;
     const { data, error } = useSWR(path, fetcher);
 
     return { items: data || [], loading: !error && !data };
