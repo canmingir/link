19dd4016712d092db52239a12d5f4fe0b7c513fe
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-08-07 16:01:52 +0300

    Add getPermittedUsers to user Instance

--- src/http/user.js
+++ src/http/user.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import config from "../config/config.js";
+import http from "./index";
 import { storage } from "@nucleoidjs/webstorage";
 
 const instance = axios.create({
@@ -64,6 +65,28 @@ instance.getUserDetails = async () => {
     }
   }
   return null;
+};
+
+instance.getPermittedUsers = async () => {
+  const userIds = [];
+  const refreshToken = await storage.get(getProjectName(), "refreshToken");
+  const response = await http.get("/permissions");
+
+  response.data.forEach((permission) => {
+    userIds.push(permission.userId);
+  });
+
+  const users = await Promise.all(
+    userIds.map(async (userId) => {
+      const response = await axios.get(
+        `https://api.github.com/user/${userId}`,
+        { headers: { Authorization: `Bearer ${refreshToken}` } }
+      );
+      return response.data;
+    })
+  );
+
+  return users;
 };
 
 export default instance;
