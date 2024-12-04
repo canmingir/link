d7ae19683081e8f734f7f3b88ca07fc1887a3faf
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-12-04 22:37:14 +0300

    Remove header from simple layout

--- src/layouts/SimpleLayout/SimpleLayout.jsx
+++ src/layouts/SimpleLayout/SimpleLayout.jsx
@@ -1,35 +1,8 @@
-import Header from "../common/header-simple";
 import { Outlet } from "react-router";
 import React from "react";
-import config from "../../../../../../config";
-import { useContext } from "../../ContextProvider/ContextProvider";
 
 export default function SimpleLayout() {
-  const [state, dispatch] = useContext();
-  const [selectedItem, setSelectedItem] = React.useState();
-
-  // TODO config.itemsData is not defined ??
-  React.useEffect(() => {
-    const foundItem = config.itemsData.find((item) => item.id === state.itemId);
-
-    if (foundItem) {
-      setSelectedItem(foundItem);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.itemId]);
-  const handleItemSelect = (item) => {
-    dispatch({ type: "ITEM_SELECT", payload: item.id });
-  };
   return (
-    <>
-      <Header
-        handleItemSelect={handleItemSelect}
-        selectedItem={selectedItem}
-        setSelectedItem={setSelectedItem}
-      />
-
       <Outlet />
-    </>
   );
 }
