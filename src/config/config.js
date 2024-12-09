f51d99e2b313985cc05dc559c1ccdda9fdbf188e
Halil Ibrahim Cengel <halilibrahimcengel@icloud.com>
2024-12-09 18:26:48 +0300

    Revert "Implement optional config.login to init function"
    This reverts commit 2be3bd5fb781c039a0dab50462dab5cad1747e36.

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,8 @@
 import {
   ConfigSchema,
-  LoginConfigSchema,
   MenuConfigSchema,
   TemplateConfigSchema,
 } from "./schemas.js";
-
 import configMain from "../../../../../config.js";
 import configMenu from "../../../../../config.menu.js";
 import configTemplate from "../../../../../config.template.js";
@@ -13,7 +11,6 @@ import { publish } from "@nucleoidai/react-event";
 let _mainConfig = {};
 let _menuConfig = {};
 let _templateConfig = {};
-let _loginConfig = {};
 
 function init() {
   const { value: mainConfig, error: errorConfig } =
@@ -22,43 +19,28 @@ function init() {
     MenuConfigSchema.validate(configMenu);
   const { value: templateConfig, error: errorTemplate } =
     TemplateConfigSchema.validate(configTemplate);
-
   if (errorConfig || errorMenu || errorTemplate) {
     publish("CONFIG_INITIALIZE_FAILED", {
       error: errorConfig?.stack || errorMenu?.stack || errorTemplate?.stack,
       file: errorMenu ? "config.menu.js" : "config.template.js",
     });
-    return;
   }
 
   _mainConfig = mainConfig;
   _menuConfig = menuConfig;
   _templateConfig = templateConfig;
 
-  Promise.resolve().then(() => {
-    try {
-      const loginModule = require("../../../../../config.login.js");
-      if (loginModule.default) {
-        const { value: loginConfig, error: errorLogin } =
-          LoginConfigSchema.validate(loginModule.default);
-        if (!errorLogin) {
-          _loginConfig = loginConfig;
-        }
-      }
-    } catch (e) {
-      console.log("Login config not found, continuing without authentication");
-    }
-    publish("CONFIG_INITIALIZED", mainConfig);
-  });
+  publish("CONFIG_INITIALIZED", mainConfig);
 }
 
 function config() {
-  return {
+  const config = {
     ..._mainConfig,
     menu: _menuConfig,
     template: _templateConfig,
-    login: _loginConfig,
   };
+
+  return config;
 }
 
 export default config;
