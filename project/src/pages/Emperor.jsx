f869f6d321c1aee48cbf06dcbd548dbc9125759b
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2024-12-27 09:40:37 +0300

    Update platform project align with new platform

--- project/src/pages/Emperor.jsx
+++ project/src/pages/Emperor.jsx
@@ -6,17 +6,9 @@ import useEmperor from "../hooks/useEmperor";
 import { useStorage } from "@nucleoidjs/webstorage";
 
 const Emperor = () => {
-  const { getEmperorById } = useEmperor();
-  const [itemId] = useStorage("itemId", null);
+  const [itemId] = useStorage("projectId", null);
 
-  const { emperor, loading, error } = getEmperorById(itemId);
-  if (loading) {
-    return <>loading</>;
-  }
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const { emperor } = useEmperor(itemId);
 
   return (
     <>
@@ -39,19 +31,18 @@ const Emperor = () => {
         >
           <Box
             component={"img"}
-            src={emperor.data.portrait}
+            src={emperor.portrait}
             sx={{ width: "10rem", borderWidth: 2, borderRadius: "2rem" }}
           />
-          <Typography variant="h2">{emperor.data.name}</Typography>
-          <Typography variant="subtitle1">
-            Reign: {emperor.data.reign}
-          </Typography>
-          <Typography variant="subtitle2">Born: {emperor.data.born}</Typography>
+          <Typography variant="h2">{emperor.name}</Typography>
+          <Typography variant="subtitle1">Reign: {emperor.reign}</Typography>
+          <Typography variant="subtitle2">Born: {emperor.born}</Typography>
           <Stack width={"70rem"} m={2}>
-            <Typography variant="body1">{emperor.data.description}</Typography>
+            <Typography variant="body1">{emperor.description}</Typography>
           </Stack>
         </Stack>
       </Box>
+      Emperor
     </>
   );
 };
