7f5abbf24fbf12208286b7ca1d49990bd12400b2
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-01-19 16:13:41 +0300

    Create neon styles

---
+++ src/components/logo/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  neon: {
+    color: "#dfdfdf",
+    textShadow: `
+        0 0 5px #209958,
+        0 0 10px #209958,
+        0 0 20px #209958,
+        0 0 40px #209958,
+        0 0 80px #209958,
+        0 0 90px #209958,
+        0 0 100px #209958,
+        0 0 150px #209958
+      `,
+  },
+};
+
+export default styles;
