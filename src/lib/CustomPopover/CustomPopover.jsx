01646bf580981fb6a1323d832196919baad3a74e
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2025-10-06 15:02:45 +0300

    Add reusable UI components and hooks to lib
    Introduces several new reusable components to the src/lib directory, including CustomBreadcrumbs, CustomPopover, FormProvider, Image, Label, RHFTextfield, Scrollbar, SearchNotFound, TableHeadCustom, and TableSelectedAction. Also adds useTable and useChart hooks. Updates src/lib/index.js to export all new modules for easier consumption throughout the project.

---
+++ src/lib/CustomPopover/CustomPopover.jsx
@@ -0,0 +1,46 @@
+import Popover from "@mui/material/Popover";
+import React from "react";
+import { StyledArrow } from "./styles";
+import { getPosition } from "./utils";
+import { menuItemClasses } from "@mui/material/MenuItem";
+
+export default function CustomPopover({
+  open,
+  children,
+  arrow = "top-right",
+  hiddenArrow,
+  sx,
+  ...other
+}) {
+  const { style, anchorOrigin, transformOrigin } = getPosition(arrow);
+
+  return (
+    <Popover
+      open={Boolean(open)}
+      anchorEl={open}
+      anchorOrigin={anchorOrigin}
+      transformOrigin={transformOrigin}
+      slotProps={{
+        paper: {
+          sx: {
+            width: "auto",
+            overflow: "inherit",
+            ...style,
+            [`& .${menuItemClasses.root}`]: {
+              "& svg": {
+                mr: 2,
+                flexShrink: 0,
+              },
+            },
+            ...sx,
+          },
+        },
+      }}
+      {...other}
+    >
+      {!hiddenArrow && <StyledArrow arrow={arrow} />}
+
+      {children}
+    </Popover>
+  );
+}
---
+++ src/lib/FormProvider/FormProvider.jsx
@@ -0,0 +1,16 @@
+import { FormProvider as Form } from "react-hook-form";
+import PropTypes from "prop-types";
+
+export default function FormProvider({ children, onSubmit, methods }) {
+  return (
+    <Form {...methods}>
+      <form onSubmit={onSubmit}>{children}</form>
+    </Form>
+  );
+}
+
+FormProvider.propTypes = {
+  children: PropTypes.node,
+  methods: PropTypes.object,
+  onSubmit: PropTypes.func,
+};
