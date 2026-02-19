d08fc7e1f6f66b68eb237ffcd38620f2a298f936
Ebubekir Yılmaz <ebu1205ed@gmail.com>
2026-02-19 16:03:01 +0300

    Add NewAPIBody and ParamView components
    Introduce NewAPIBody: a side-by-side request/response panel built with MUI that renders SchemaEditor instances for request/response (wired to requestSchemaRef/responseSchemaRef) and falls back to ParamView for GET methods. Add ParamView: a small table component that lists parameter name/type pairs (uses uuid for row keys). Both components include data-cy attributes for testing and accept custom types via props.

---
+++ src/lib/NewApiBody/NewAPIBody.jsx
@@ -0,0 +1,97 @@
+import ParamView from "./ParamView";
+import React from "react";
+import SchemaEditor from "../SchemaEditor";
+
+import { Box, Divider, Paper, Typography } from "@mui/material";
+
+const NewAPIBody = ({ types, api, requestSchemaRef, responseSchemaRef }) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        height: "85%",
+        p: 2,
+      }}
+      data-cy="api-body"
+    >
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            width: "100%",
+            p: 2,
+            bgcolor: "background.paper",
+            borderRadius: 2,
+            height: "100%",
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          data-cy="request-schema-editor"
+        >
+          {api.method === "GET" ? (
+            <Box sx={{ width: "100%", height: "100%" }}>
+              <ParamView params={api.params} />
+            </Box>
+          ) : (
+            <SchemaEditor
+              ref={requestSchemaRef}
+              initialData={api.request ? api.request.schema : ""}
+              customTypes={types}
+            />
+          )}
+          <Typography variant="h6" gutterBottom>
+            Request
+          </Typography>
+        </Paper>
+      </Box>
+
+      <Divider orientation="vertical" flexItem sx={{ width: "1rem" }} />
+
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            width: "100%",
+            p: 2,
+            bgcolor: "background.paper",
+            borderRadius: 2,
+            height: "100%",
+            boxSizing: "border-box",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          data-cy="response-schema-editor"
+        >
+          <SchemaEditor
+            ref={responseSchemaRef}
+            customTypes={types}
+            initialData={api.response ? api.response.schema : ""}
+          />
+          <Typography variant="h6" gutterBottom>
+            Response
+          </Typography>
+        </Paper>
+      </Box>
+    </Box>
+  );
+};
+
+export default NewAPIBody;
---
+++ src/lib/NewApiBody/ParamView.jsx
@@ -0,0 +1,38 @@
+import { v4 as uuid } from "uuid";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+function ParamView({ params }) {
+  params = params || [];
+
+  return (
+    <Table size={"small"} data-cy="param-view">
+      <colgroup>
+        <col style={{ width: "50%" }} />
+        <col style={{ width: "50%" }} />
+      </colgroup>
+      <TableHead>
+        <TableRow>
+          <TableCell>Parameter</TableCell>
+          <TableCell>Data Type</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {params.map((param) => (
+          <TableRow key={uuid()}>
+            <TableCell>{param.name}</TableCell>
+            <TableCell>{param.type}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+export default ParamView;
